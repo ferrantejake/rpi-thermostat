8c8b0bf9820e7735954df87afb58f7f44e5a78be
Jake Ferrante <jake.ferrante@hotmail.com>
2016-06-04 02:49:53 -0400

    Removes "rpi-gpio" in favor of "onoff", the prelim does not seem to be supported for RPi3 yet..

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var gpio = require('rpi-gpio');
-const PIN  = 4;
-
-gpio.setup(PIN, gpio.DIR_OUT, write);
- 
-function write() {
-    gpio.write(PIN, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
-}
+var app = require('./src/app');
